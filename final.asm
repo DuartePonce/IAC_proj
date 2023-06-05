6b25fdc57e15ef08c34ab88a876e949a38c63c8b
DuartePonce <duarteponce4@gmail.com>
2023-06-05 11:23:47 +0100

    h

--- final.asm
+++ final.asm
@@ -54,7 +54,7 @@ REPRODUCAO               EQU COMANDOS + 5AH     ; Reproduz o audio
 ;*********************************************************************************
 ;Zona de dados
 ;*********************************************************************************
-PLACE 1000H
+PLACE 2000H
 painel_lista:
 
     WORD 0,0,0, ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO, 0,0,0
@@ -82,7 +82,7 @@ sonda:
     WORD VERDE
 
 tab:
-    WORD    asteroide
+    WORD    0   
     WORD    0
     WORD    0   
     WORD    0
@@ -90,10 +90,16 @@ tab:
 ;*********************************************************************************
 
 ;*********************************************************************************
-PLACE 2000H
-pilha:
-	STACK 500H		
+
+	STACK 100H		
 SP_inicial:	
+
+	STACK 100H		; espaço reservado para a pilha do processo "teclado"
+SP_teclado:			; este é o endereço com que o SP deste processo deve ser inicializado
+
+
+tecla_carregada:
+	LOCK 0
 ;*********************************************************************************
 ;Codigo principal
 ;*********************************************************************************
@@ -113,39 +119,48 @@ display:
 menu_principal:
     MOV	 R1, IMAGEM1			                    ; cenário de fundo número 0
     MOV  [SELECIONA_CENARIO_FUNDO], R1	            ; seleciona o cenário de fundo
-inicio_jogo:
-    CALL  inicio
 
-fim_jogo:
-    ; meter imagem de fim de jogo
-    JMP inicio_jogo
+    CALL	teclado
 
+
+
+
+	MOV  R2, 0			; valor do contador, cujo valor vai ser mostrado nos displays
+	MOV  R0, DISPLAYS        ; endereço do periférico que liga aos displays
+atualiza_display:	
+	MOVB [R0], R2            ; mostra o valor do contador nos displays
+obtem_tecla:	
+	MOV	R1, [tecla_carregada]	; bloqueia neste LOCK até uma tecla ser carregada
+    MOV R3, 81H
+	CMP	R1, R3		; é a coluna da tecla C?
+	JNZ	testa_D
+	ADD  R2, 1               ; aumenta o contador
+	JMP	atualiza_display
+testa_D:	
+    MOV R3, 82H 
+	CMP	R1, R3		; é a coluna da tecla D?
+	JNZ	obtem_tecla		; se não, ignora a tecla e espera por outra
+	SUB  R2, 1               ; diminui o contador
+	JMP	atualiza_display	; processo do programa principal nunca termina
 ;*********************************************************************************
-; Teclado - o codigo que se segue tem como objetivo ler os inputs do teclado
-;           e avaliar o seu conteudo
+; teclado
 ;*********************************************************************************
+PROCESS SP_teclado 
 
-inicio:
-    PUSH R0
-    PUSH R1
-    PUSH R2
-    PUSH R3
-    PUSH R4
-    PUSH R5
-    PUSH R6
-    PUSH R7
-
+teclado:
     MOV  R6, LINHA
     MOV  R2, TEC_LIN   ; endere�o do perif�rico das linhas
     MOV  R3, TEC_COL   ; endere�o do perif�rico das colunas
     MOV  R4, 2
     MOV  R5, MASCARA   ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
 
-    MOV R11, 0
 ciclo:
     MOV  R1, 0
 
-espera_tecla:          ; neste ciclo espera-se at� uma tecla ser premida
+espera_tecla:        ; neste ciclo espera-se at� uma tecla ser premida
+
+    YIELD
+
     MOV  R1, 10H
     CMP  R1, R6   
     JZ   reset
@@ -157,183 +172,24 @@ espera_tecla:          ; neste ciclo espera-se at� uma tecla ser premida
     CMP  R0, 0         ; h� tecla premida?
 
 
-    JZ   espera_tecla  ; se nenhuma tecla premida, repete
+    JZ   espera_tecla       ; se nenhuma tecla premida, repete
 
-    SHL  R1, 4         ; coloca linha no nibble high
-    OR   R1, R0        ; junta coluna (nibble low)
-
+    SHL  R1, 4              ; coloca linha no nibble high
+    OR   R1, R0             ; junta coluna (nibble low)
+    MOV	[tecla_carregada], R1
     
 ha_tecla:              ; neste ciclo espera-se at� NENHUMA tecla estar premida
+
+    YIELD
+
     MOVB R0, [R3]      ; ler do perif�rico de entrada (colunas)
     AND  R0, R5        ; elimina bits para al�m dos bits 0-3
     CMP  R0, 0         ; h� tecla premida?
     JNZ  ha_tecla      ; se ainda houver uma tecla premida, espera at� n�o haver
 
-    JMP  ver_tecla
+    JMP ciclo
 
 reset:
     MOV  R6, LINHA
     JMP espera_tecla
 
-ver_tecla: 
-
-    MOV R7, 0081H         ; R7 caso da tecla C
-    CMP R1, R7
-    JZ tecla_C
-
-    JMP ciclo
-tecla_C:
-    CALL  fundo_jogo
-    EI0
-    EI
-    JMP ciclo
-
-fim_teclado:
-    POP R7
-    POP R6
-    POP R5
-    POP R4
-    POP R3
-    POP R2
-    POP R1
-    POP R0    
-    RET
-;*********************************************************************************
-;Codigo painel - codigo que desenha e da setup ao painel de jogo mudando a imagem 
-;                de fundo e desenhando o painel
-;*********************************************************************************
-
-fundo_jogo:
-    PUSH R0
-    PUSH R1
-    PUSH R2
-    PUSH R3
-    PUSH R4
-    PUSH R5
-    PUSH R6
-
-    MOV	 R1, IMAGEM2			                    ; cenário de fundo número 1
-    MOV  [SELECIONA_CENARIO_FUNDO], R1	            ; seleciona o cenário de fundo
-
-posicao_painel:
-    MOV  R1, LIN
-    MOV  R2, COL 
-	MOV	 R4, painel_lista		; endereço da tabela que define o painel
-
-    MOV  R5, LARGURA			    ; largura do painel
-    MOV  R6, ALTURA               ; guarda a altura do painel
-
-desenha_pixels:       		    ; desenha os pixels do painel a partir da tabela
-	MOV	 R3, [R4]			    ; obtém a cor do próximo pixel do painel
-	MOV  [DEFINE_LINHA], R1	    ; seleciona a linha
-	MOV  [DEFINE_COLUNA], R2	; seleciona a coluna
-	MOV  [DEFINE_PIXEL], R3	    ; altera a cor do pixel na linha e coluna selecionadas
-	ADD	 R4, 2			        ; endereço da cor do próximo pixel (2 porque cada cor de pixel é uma word)
-    ADD  R2, 1                  ; próxima coluna
-    SUB  R5, 1			        ; menos uma coluna para tratar
-    JNZ  desenha_pixels         ; continua até percorrer toda a largura do objeto
-
-linha_pixel_seg:
-    MOV  R5, LARGURA             ; reseta o valor de r5 com r8
-    MOV  R2, COL            ; reseta o valor da coluna para r2
-    ADD  R1, 1              ; adiciona um ao r1 para avaancar a linha
-    SUB  R6, 1             ; subtrai o numero de linhas ate chegar a zero
-    JNZ  desenha_pixels
-    
-    POP R6
-    POP R5
-    POP R4
-    POP R3
-    POP R2
-    POP R1
-    POP R0
-    RET
-
-
-;*********************************************************************************
-; Asteroide
-;*********************************************************************************
-
-;falyta descobrir como e onde desenhar
-asteroide:
-    PUSH R0
-    PUSH R1
-    PUSH R2
-    PUSH R3
-    PUSH R4
-    PUSH R5
-    PUSH R6
-
-ciclo_asteroide:
-
-    MOV R1, R11
-    MOV R2, R11
-    ;R1(linha) e R2 sao as coordenadas de desenho ns quais
-    MOV R4, TAMANHO_ASTEROIDE
-    MOV R5, TAMANHO_ASTEROIDE ;LINHA
-    MOV R6, 0
-
-qual_asteroide:
-    ;cena pseudo aleatoria para ver qual desenhaar
-    ; DEPOIS FAZER COMPARACAO PARA VER QUAL TEMPLATE USARA NO DESENHO
-    MOV R0, asteroide_n_mineravel
-    CMP R1, 0
-    JZ desenhar_asteroide
-
-   ; MOV R0, asteroide_mineravel
-
-    JMP apagar_asteroide
-
-
-apagar_asteroide:
-    MOV R3, 0
-    MOV [DEFINE_LINHA], R1	    ; seleciona a linha
-	MOV [DEFINE_COLUNA], R2	; seleciona a coluna
-	MOV [DEFINE_PIXEL], R3	    ; altera a cor do pixel na linha e coluna selecionadas
-    ADD R2, 1
-    SUB R4, 1
-    JNZ apagar_asteroide
-    CMP R5, 0
-    JNZ linha_seguinte_asteroide
-desenhar_asteroide:
-    MOV R3, [R0]
-    MOV [DEFINE_LINHA], R1	    ; seleciona a linha
-	MOV [DEFINE_COLUNA], R2	; seleciona a coluna
-	MOV [DEFINE_PIXEL], R3	    ; altera a cor do pixel na linha e coluna selecionadas
-    ADD R2, 1
-    ADD R0, 2
-    SUB R4, 1
-    JNZ desenhar_asteroide
-    CMP R5, 0
-    JNZ linha_seguinte_asteroide
-
-incrementacao:
-
-
-linha_seguinte_asteroide:
-
-    MOV R2, R11
-    ;resetar r2
-
-    MOV R4, TAMANHO_ASTEROIDE
-    SUB R5, 1
-    ADD R1, 1
-    ADD R6, 1
-
-    CMP R6, 6  ; ideias de comparacao
-    JGT  apagar_asteroide
-
-    MOV R5, TAMANHO_ASTEROIDE ; para assim se desenhhar
-    JLT desenhar_asteroide
-    ;comparacao para saltar para o desenhar USA O JGT
-    
-fim_asteroide:
-    ADD R11, 1
-    POP R6
-    POP R5
-    POP R4
-    POP R3
-    POP R2
-    POP R1
-    POP R0
-    RFE
