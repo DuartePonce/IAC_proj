c7480685c70c8893fff119e111d235d564eb6759
Antonio <antonioprodelgado03@gmail.com>
2023-06-06 00:34:14 +0100

    yay

--- projeto.asm
+++ projeto.asm
@@ -1,4 +1,11 @@
 ;*********************************************************************************
+; Grupo-1
+; Duarte Ponce, Antonio Delgado e Rodrigo Salgueiro
+; 107489, 106658, 106505
+;*********************************************************************************
+
+
+;*********************************************************************************
 ;Constantes principais a declarar:
 ;*********************************************************************************
 
@@ -11,8 +18,7 @@ N_LINHAS   EQU  32	        ; número de linhas do ecrã (altura)
 N_COLUNAS  EQU  64	        ; número de colunas do ecrã (largura)
 LARGURA    EQU 16           ; Largura do painel
 ALTURA     EQU 6            ; Altura do painel
-LARGURA_ASTEROIDE    EQU 5  ; Largura do asteroide
-ALTURA_ASTEROIDE     EQU 5  ; Altura do asteroide
+TAMANHO_ASTEROIDE    EQU 5  ; Tamanho do asteroide
 LIN        EQU 26           ; Linha inicial de desenho do painel
 COL        EQU 24           ; Coluna inicial do desenho do painel
 IMAGEM1    EQU 0            
@@ -20,6 +26,9 @@ IMAGEM2    EQU 1
 COLUNA_SONDA    EQU 32      ; Posicao inical da sona
 LINHA_SONDA     EQU 25      ; Posicao inicial da sonda
 SOM1            EQU 0
+SONDA1_CORD    EQU 25
+
+ENERGIA_INICIAL EQU 103
 
 ;*********************************************************************************
 ;Cores
@@ -48,10 +57,9 @@ REPRODUCAO               EQU COMANDOS + 5AH     ; Reproduz o audio
 ;*********************************************************************************
 ;Zona de dados
 ;*********************************************************************************
-PLACE 1000H
+PLACE 2000H
 painel_lista:
-    WORD LARGURA
-    WORD ALTURA
+
     WORD 0,0,0, ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO, ROXO_ESCURO, 0,0,0
     WORD 0, ROXO_ESCURO, ROXO_ESCURO, ROXO_CLARO, ROXO_CLARO, ROXO_CLARO, ROXO_CLARO, ROXO_CLARO, ROXO_CLARO, ROXO_CLARO, ROXO_CLARO, ROXO_CLARO, ROXO_CLARO, ROXO_ESCURO, ROXO_ESCURO, 0
     WORD 0, ROXO_ESCURO, ROXO_CLARO, CINZENTO, CINZENTO, CINZENTO, CINZENTO, CINZENTO, CINZENTO, CINZENTO, CINZENTO, CINZENTO, CINZENTO, ROXO_CLARO, ROXO_ESCURO, 0
@@ -60,122 +68,130 @@ painel_lista:
     WORD ROXO_ESCURO, ROXO_CLARO, ROXO_CLARO, CINZENTO, CINZENTO, CINZENTO, CINZENTO, CINZENTO, CINZENTO, CINZENTO, PRETO, CINZENTO, CINZENTO, ROXO_CLARO, ROXO_CLARO, ROXO_ESCURO
 
 asteroide_n_mineravel:
-    WORD LARGURA_ASTEROIDE
-    WORD ALTURA_ASTEROIDE
     WORD VERMELHO, 0,0,0, VERMELHO
     WORD VERMELHO, 0, VERMELHO, 0, VERMELHO
     WORD VERMELHO, VERMELHO, PRETO, VERMELHO, VERMELHO
     WORD VERMELHO, 0, VERMELHO, 0, VERMELHO
     WORD VERMELHO, 0,0,0, VERMELHO
 
+asteroide_mineravel:
+    WORD 0
+    WORD 0
+    WORD 0
+    WORD 0
+    WORD 0
+
 sonda:
     WORD VERDE
+
+tab:
+    WORD    0 
+    WORD    interrupcao_sonda
+    WORD    interrupcao_energia   
+    WORD    0
+
+sondas:
+    WORD SONDA1_CORD, SONDA1_CORD
+    WORD 0, 0
+    WORD 0, 0
+
+energia_nave:
+    WORD ENERGIA_INICIAL
+;*********************************************************************************
+; Stacks 
+;*********************************************************************************
+
+	STACK 100H		
+SP_inicial:	
+
+	STACK 100H		; espaço reservado para a pilha do processo "teclado"
+SP_teclado:			; este é o endereço com que o SP deste processo deve ser inicializado
+
+    STACK 100H
+SP_sonda1:
+
+    STACK 100H
+SP_energia:
+
+tecla_carregada:
+	LOCK 0
+sonda1:
+    LOCK 0
+energia:
+    LOCK 0
 ;*********************************************************************************
 ;Codigo principal
 ;*********************************************************************************
 PLACE      0
 
 apaga_imagem:
+    MOV  SP, SP_inicial
+    MOV  BTE, tab
     MOV  [APAGA_AVISO], R1          ; apaga o aviso de nenhum cenário selecionado (o valor de R1 não é relevante)
-    MOV  [APAGA_ECRÃ], R1	        ; apaga todos os pixels já desenhados (o valor de R1 não é relevante)
-display:
-    MOV  R0, DISPLAYS               ; Funcao que da setup ao display
-    MOV  R9, 100H                       
-    MOV  [R0], R9
-
-
+    MOV  [APAGA_ECRÃ], R1	        ; apaga todos os pixels já desenhados (o valor de R1 não é relevante)                
 menu_principal:
     MOV	 R1, IMAGEM1			                    ; cenário de fundo número 0
     MOV  [SELECIONA_CENARIO_FUNDO], R1	            ; seleciona o cenário de fundo
-    MOV R6, LINHA                                   ; instrucao necessaria ao comeco do codigo seguinte
+    CALL teclado
+obtem_tecla:	
+	MOV	R1, [tecla_carregada]	; bloqueia neste LOCK até uma tecla ser carregada
+    
+    MOV R3, 81H   
+    CMP	R1, R3		; é a coluna da tecla C?
+	JZ	testa_C
+
+testa_C:
+    CALL fundo_jogo
+    CALL sonda_1
+    CALL energia_inicio
+    EI1
+    EI2
+    EI
 
 ;*********************************************************************************
-; Teclado - o codigo que se segue tem como objetivo ler os inputs do teclado
-;           e avaliar o seu conteudo
+; teclado
 ;*********************************************************************************
 
-teclado_var:           ; inicializacao dos registos referentes ao teclado
-    MOV  R2, TEC_LIN   ; endereco do periferico das linhas
-    MOV  R3, TEC_COL   ; endereco do periferico das colunas
-    MOV  R4, DISPLAYS  ; endereco do periferico dos displays
+PROCESS SP_teclado 
+
+teclado:
+    MOV  R6, LINHA
+    MOV  R2, TEC_LIN   ; endere�o do perif�rico das linhas
+    MOV  R3, TEC_COL   ; endere�o do perif�rico das colunas
     MOV  R5, MASCARA   ; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
-    MOV  R7, 2         ; multiplicador
-    MOV  R8, 8         ; constante de comparacao
-    MOV  R1, R6   
-    MOVB [R2], R1      ; escrever no periferico de saida (linhas)
-    MOVB R0, [R3]      ; ler do periferico de entrada (colunas)
-    AND  R0, R5        ; elimina bits para alem dos bits 0-3
-    CMP  R0, 0         ; ha tecla premida?
-    JNZ teclado_var
 
+ciclo:
+    MOV  R1, 0
 
-reset_linha:
-    MOV R6, LINHA
+espera_tecla:        ; neste ciclo espera-se at� uma tecla ser premida
+    YIELD
+    MOV  R1, 10H
+    CMP  R1, R6   
+    JZ   reset
+    MOV  R1, R6 
+    MOVB [R2], R1      ; escrever no perif�rico de sa�da (linhas)
+    MOVB R0, [R3]      ; ler do perif�rico de entrada (colunas)
+    AND  R0, R5        ; elimina bits para al�m dos bits 0-3
+    SHL  R6, 1
+    CMP  R0, 0         ; h� tecla premida?
 
-espera_tecla: 
+    JZ   espera_tecla       ; se nenhuma tecla premida, repete
 
-    MOV  R1, R6   
-    MOVB [R2], R1      ; escrever no periferico de saida (linhas)
-    MOVB R0, [R3]      ; ler do periferico de entrada (colunas)
-    AND  R0, R5        ; elimina bits para alem dos bits 0-3
-    CMP  R0, 0         ; ha tecla premida?
-    JZ linha_seguinte
+    SHL  R1, 4              ; coloca linha no nibble high
+    OR   R1, R0             ; junta coluna (nibble low)
+    MOV	[tecla_carregada], R1 ; desbloquia teclado
+    
+ha_tecla:              ; neste ciclo espera-se at� NENHUMA tecla estar premida
+    YIELD 
+    MOVB R0, [R3]      ; ler do perif�rico de entrada (colunas)
+    AND  R0, R5        ; elimina bits para al�m dos bits 0-3
+    CMP  R0, 0         ; h� tecla premida?
+    JNZ  ha_tecla      ; se ainda houver uma tecla premida, espera at� n�o haver
+    JMP ciclo
 
-    SHL  R1, 4         ; coloca linha no nibble high 
-    OR   R1, R0        ; junta coluna (nibble low)
-
-    JMP ver_tecla            
-
-linha_seguinte:
-    CMP R6, R8          ; caso r8 seja igual ao r6 sera necessario resetar a linha
-    JZ reset_linha
-    MUL  R6, R7         ; avancar a linha
-    JMP   espera_tecla
-
-ver_tecla:
-    MOV R8, 0011H         ; R8 caso da tecla 0
-    CMP R1, R8
-    JZ tecla_1
-
-    MOV R8, 0012H         ; R8 caso da tecla 1
-    CMP R1, R8
-    JZ tecla_2
-
-    MOV R8, 0014H         ; R8 caso da tecla 2
-    CMP R1, R8
-    JZ tecla_3
-
-    MOV R8, 0018H         ; R8 caso da tecla 3
-    CMP R1, R8
-    JZ tecla_4
-
-    MOV R8, 0081H         ; R8 caso da tecla C
-    CMP R1, R8
-    JZ tecla_5
-
-    JMP teclado_var    ; jump feito para resetar o valor dos registros caso nenhuma das teclas antecipadas seja premida
-
-
-tecla_1:
-    JMP apaga_asteroide 
-
-tecla_2:
-    JMP mata_sonda
-
-tecla_3:                    ; Tecla de incrementacao ao display
-    MOV  R0, DISPLAYS
-    ADD  R9, 001H
-    MOV  [R0], R9
-    JMP  teclado_var
-
-tecla_4:                    ; Tecla que decrementa ao display
-    MOV  R0, DISPLAYS
-    SUB  R9, 001H
-    MOV  [R0], R9
-    JMP  teclado_var
-
-tecla_5:
-    JMP  fundo_jogo
+reset:
+    MOV  R6, LINHA
+    JMP espera_tecla
 
 ;*********************************************************************************
 ;Codigo painel - codigo que desenha e da setup ao painel de jogo mudando a imagem 
@@ -183,22 +199,23 @@ tecla_5:
 ;*********************************************************************************
 
 fundo_jogo:
+    PUSH R0
+    PUSH R1
+    PUSH R2
+    PUSH R3
+    PUSH R4
+    PUSH R5
+    PUSH R6
+
     MOV	 R1, IMAGEM2			                    ; cenário de fundo número 1
     MOV  [SELECIONA_CENARIO_FUNDO], R1	            ; seleciona o cenário de fundo
-    MOV  R11, 0                                     ; Registo reservado para o asteroide
-    MOV  R10, 25                                    ; Registo reservado para a sonda
-
 posicao_painel:
     MOV  R1, LIN
     MOV  R2, COL 
 	MOV	 R4, painel_lista		; endereço da tabela que define o painel
-    MOV  R5, [R4]			    ; linha do painel
-    MOV  R7, [R4]               ; guarda a linha do painel
-    ADD  R7, R2                 ; soma a coluna inicial ao tamanho da linha para saber as dimensoes maximas
-    MOV  R8, [R4]               ; guarda a linha do painel
-    ADD	 R4, 2                  
-    MOV  R6, [R4]	            ; coluna do painel
-    ADD	 R4, 2                  
+
+    MOV  R5, LARGURA			    ; largura do painel
+    MOV  R6, ALTURA               ; guarda a altura do painel
 
 desenha_pixels:       		    ; desenha os pixels do painel a partir da tabela
 	MOV	 R3, [R4]			    ; obtém a cor do próximo pixel do painel
@@ -211,117 +228,93 @@ desenha_pixels:       		    ; desenha os pixels do painel a partir da tabela
     JNZ  desenha_pixels         ; continua até percorrer toda a largura do objeto
 
 linha_pixel_seg:
-    MOV  R5, R8             ; reseta o valor de r5 com r8
+    MOV  R5, LARGURA             ; reseta o valor de r5 com r8
     MOV  R2, COL            ; reseta o valor da coluna para r2
     ADD  R1, 1              ; adiciona um ao r1 para avaancar a linha
-    CMP  R1, R7             ; e compara o r1 com o r7 para verificar se ja chegamos a ultima linha 
+    SUB  R6, 1             ; subtrai o numero de linhas ate chegar a zero
     JNZ  desenha_pixels
-
-
-
-;*********************************************************************************
-;Asteroide - codigo que desenha o asteroide(Tie fighter) pixel a pixel
-;*********************************************************************************
-
-
-posicao_asteroide:
-    MOV  R1, R11                        ; defenir linha
-    MOV  R2, R11                        ; defenir coluna
-	MOV	 R4, asteroide_n_mineravel	    ; endereço da tabela que define o asteroide
-    MOV  R5, [R4]			            ; comprimento do asteroide
-    MOV  R8, [R4]                       ; altura do asteroide
-    ADD	 R4, 2
-    MOV  R6, [R4]	                    ; coluna do asteroide
-    MOV  R0, [R4]                       ; isto serve para a comparacao que para de o desenhar tipo isso
-    ADD  R0, R1                         ; serve para a comparacao tb
-    ADD	 R4, 2
     
-desenha_pixels_as:       		        ; desenha os pixels do asteroide a partir da tabela
-	MOV	 R3, [R4]			            ; obtém a cor do próximo pixel do asteroide
-	MOV  [DEFINE_LINHA], R1	            ; seleciona a linha
-	MOV  [DEFINE_COLUNA], R2	        ; seleciona a coluna
-	MOV  [DEFINE_PIXEL], R3	            ; altera a cor do pixel na linha e coluna selecionadas
-	ADD	 R4, 2			                ; endereço da cor do próximo pixel (2 porque cada cor de pixel é uma word)
-    ADD  R2, 1                          ; próxima coluna
-    SUB  R5, 1			                ; menos uma coluna para tratar
-    JNZ  desenha_pixels_as              ; continua até percorrer toda a largura do objeto
-    
-linha_pixel_seg_as:             ; segue a mesma logica do painel 
-    MOV  R5, R8                 
-    MOV  R2, R11                ; reseta o valor do R2 com o valor da coluna onde comeca a desenhar
-    ADD  R1, 1                  ; incrmenta um ao r1 para avancar a linha
-    CMP  R0, R1
-    JNZ  desenha_pixels_as
-    MOV  R8, LINHA_SONDA
-    CMP  R10, R8                ; avanca para a sonda quando ela nao existe
-    JZ   cria_sonda
-    JMP  teclado_var
-
-reset_asteroide:                ; reseta o asteroide quando ele chega a ultima linha do campo de jogo
-    MOV R11, 0
-    JMP posicao_painel
-
-;*********************************************************************************
-;Apagar asteroide - codigo que apaga o desenho do asteroide 
-;*********************************************************************************
-
-apaga_asteroide:                            ; desenha o asteroide a partir da tabela
-    MOV    R1, R11                          ; defenir linha
-    MOV    R2, R11                          ; defenir coluna
-    MOV    R4, asteroide_n_mineravel        ; endereço da tabela que define o asteroide
-    MOV    R5, [R4]                         ; obtém a largura do asteroide
-    MOV    R8, [R4]
-    ADD    R8, R1
-
-apaga_pixels:                               ; desenha os pixels do asteroide a partir da tabela
-    MOV  R3, 0                              ; para apagar, a cor do pixel é sempre 0
-    MOV  [DEFINE_LINHA], R1                 ; seleciona a linha
-    MOV  [DEFINE_COLUNA], R2                ; seleciona a coluna
-    MOV  [DEFINE_PIXEL], R3                 ; altera a cor do pixel na linha e coluna selecionadas
-    ADD  R2, 1                              ; próxima coluna
-    SUB  R5, 1                              ; menos uma coluna para tratar
-    JNZ  apaga_pixels                       ; continua até percorrer toda a largura do objeto
-
-apaga_proxima_linha:
-    MOV  R5, 5                  ; R5 tem o valor do tamanho do asteroide
-    MOV  R2, R11                ; avanca para a coluna inicial
-    ADD  R1, 1
-    CMP  R1, R8                 ; comparacao para verificar se ainda ha linhas a apagar
-    JNZ  apaga_pixels            
-    ADD  R11, 1                 ; Avanca a posicao asteroide
-    MOV R8, 31
-    CMP R11, R8
-    JZ  reset_asteroide         ; caso o asteroide esteja na ultima linha do painel vai reseta lo
-    
-    MOV R8, SOM1                ; reporducao do audio do asteroide a mover se
-    MOV [REPRODUCAO], R8
-    JMP  posicao_asteroide
-
+    POP R6
+    POP R5
+    POP R4
+    POP R3
+    POP R2
+    POP R1
+    POP R0
+    RET
 
 
 ;*********************************************************************************
-;Sonda - codigo que desenha e apaga a sonda
+; Sonda
 ;*********************************************************************************
 
-cria_sonda:
-    MOV  R1, R10                             ; defenir linha
-    MOV  R2, COLUNA_SONDA                    ; defenir coluna
-    MOV  R4, sonda
-    MOV  R3, [R4]
-    MOV  [DEFINE_LINHA], R1                 ; seleciona a linha
-    MOV  [DEFINE_COLUNA], R2                ; seleciona a coluna
-    MOV  [DEFINE_PIXEL], R3                 ; altera a cor do pixel na linha e coluna selecionadas
-    JMP  teclado_var
+PROCESS SP_sonda1
+sonda_1:
+    MOV R4, sondas
+    MOV R1, [R4]
+ciclo_sonda:
+    CALL desenhar_sonda
+    MOV R2, [sonda1]    ; lê o LOCK e bloqueia até a interrupção escrever nele
+    CALL apagar_sonda
+    JMP ciclo_sonda
 
-mata_sonda:
-    MOV  R1, R10                            ; defenir linha
-    MOV  R2, COLUNA_SONDA                   ; defenir coluna
-    MOV  R3, 0
-    MOV  [DEFINE_LINHA], R1                 ; seleciona a linha
-    MOV  [DEFINE_COLUNA], R2                ; seleciona a coluna
-    MOV  [DEFINE_PIXEL], R3                 ; altera a cor do pixel na linha e coluna selecionadas
-    SUB  R10, 1
-    JMP  cria_sonda 
+desenhar_sonda:
+    MOV R3, VERDE
+	MOV  [DEFINE_LINHA], R1	    ; seleciona a linha
+	MOV  [DEFINE_COLUNA], R1	; seleciona a coluna
+	MOV  [DEFINE_PIXEL], R3	    ; altera a cor do pixel na linha e coluna selecionadas
+    RET
+apagar_sonda:
+    MOV R3, 0
+	MOV  [DEFINE_LINHA], R1	    ; seleciona a linha
+	MOV  [DEFINE_COLUNA], R1	; seleciona a coluna
+	MOV  [DEFINE_PIXEL], R3	    ; altera a cor do pixel na linha e coluna selecionadas
 
-fim:
-    JMP  fim 
+    SUB R1, 1
+    MOV [R4], R1
+
+    RET
+
+PROCESS SP_energia
+energia_inicio:
+    MOV R4, energia_nave
+    MOV R9, [R4]
+energia_purpolsores:
+    SUB R9, 3
+    CALL converte
+    MOV R1, [energia]
+    JMP energia_purpolsores
+    ; (das perdas de energia ao disparar e os ganhos ao minerar asteroides bons)
+; energia_disparo
+; energia_as_mineravel
+converte:
+    MOV R0, DISPLAYS
+    MOV R1, R9
+    MOV R3, 0
+    MOV R4, 16
+    MOV R5, 10
+    CALL converte_aux
+    MOV [R0], R3
+    RET
+converte_aux:
+    MOV R2, R1
+    MOD R2, R5
+    DIV R1, R5
+    MUL R2, R4
+    ADD R3, R2
+    SHL R4, 4
+    CMP R1, 0
+    JNZ converte_aux
+    SHR R3, 4
+    RET
+
+;*********************************************************************************
+; Interrupcoes
+;*********************************************************************************
+interrupcao_sonda:
+    MOV [sonda1], R1 ; desbloqueia processo da sonda (qualquer registo serve)
+    RFE
+
+interrupcao_energia:
+    MOV  [energia], R1
+    RFE
